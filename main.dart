4c53e9647780408fa93633e26a87add7c38ad631
code-simple <61619216+code-simple@users.noreply.github.com>
2021-08-21 00:28:15 +0500

    Initial Commit

--- main.dart
+++ main.dart
@@ -1,1 +1,14 @@
+//This small script will convert EVERY FIRST LETTER of any String to Capital.
 
+void main(){
+print(cptl('every letter will be starting with capital letter'));
+}
+
+String cptl(String txt){
+  List cpName = [];
+  for(int i=0;i<txt.split(' ').length;i++){
+  var a = (txt.split(' ')[i].toString()).split('')[0].toUpperCase();
+  cpName.add('$a${(txt.split(' ')[i].toString()).substring(1)}');
+  }
+return cpName.join(' ');
+}
